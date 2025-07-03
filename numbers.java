903f916c13142ce79efe62976ef96d73b83d3d07
root <root@ip-172-31-17-3.ec2.internal>
2025-07-03 07:36:02 +0000

    dsfdf

--- numbers.java
+++ numbers.java
@@ -1,1 +1,15 @@
-void main()
+Public class login
+{
+   public static void main()
+   {
+       int i =0;
+       
+       for (i=0;i<==60;i++     
+   
+
+
+       {
+	    System.out Println ("Number: "+i);
+       }
+   }
+}
